48e0f48ef21d1b867268567c67a428a5cafd4fb9
Jason Christopher <jason.christopher@u.nus.edu>
2023-07-21 23:57:29 +0800

    Complete Hotel Endpoint

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", indexHandler)
 	router.GET("/flight", getFlight)
+	router.GET("/hotel", getHotel)
 
 	router.Run("localhost:8080")
 }
@@ -86,7 +87,7 @@ func getFlight(c *gin.Context) {
 	if err = cursor.All(context.TODO(), &depResults); err != nil {
 		panic(err)
 	}
-	
+
 	if len(depResults) == 0 {
 		c.IndentedJSON(http.StatusNotFound, gin.H{
 			"message": "No returning flights found",
@@ -110,7 +111,6 @@ func getFlight(c *gin.Context) {
 	if err != nil {
 		panic(err)
 	}
-
 
 	// Get a list of all returned documents and print them out
 	var retResults []bson.M
@@ -139,6 +139,79 @@ func getFlight(c *gin.Context) {
 
 	if err != nil {
 		panic(err)
+	}
+
+	c.IndentedJSON(http.StatusOK, response)
+}
+
+func getHotel(c *gin.Context) {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("No MONGODB_URI environment variable found")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("minichallenge").Collection("hotels")
+
+	// Get query parameters
+	checkInDateString := c.Query("checkInDate")
+	checkOutDateString := c.Query("checkOutDate")
+	destination := c.Query("destination")
+
+	checkInDate, err := time.Parse("2006-01-02", checkInDateString)
+	checkOutDate, err := time.Parse("2006-01-02", checkOutDateString)
+
+	// Aggregate Pipeline
+	pipeline := bson.A{
+		bson.D{{"$match", bson.D{
+			{Key: "city", Value: destination},
+			{Key: "date", Value: bson.D{{Key: "$gte", Value: checkInDate}, {Key: "$lte", Value: checkOutDate}}},
+		}}},
+		bson.D{{"$group", bson.D{{"_id", "$hotelName"}, {"price", bson.D{{"$sum", "$price"}}}}}},
+		bson.D{{"$sort", bson.D{{Key: "price", Value: 1}}}},
+	}
+
+	cursor, err := coll.Aggregate(context.TODO(), pipeline)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var result []bson.M
+	if err = cursor.All(context.TODO(), &result); err != nil {
+		panic(err)
+	}
+
+	if len(result) == 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{
+			"message": "No hotels found",
+		})
+		return
+	}
+
+	hotel := result[0]
+
+	response := gin.H{
+		"City":           destination,
+		"Check In Date":  checkInDateString,
+		"Check Out Date": checkOutDateString,
+		"Hotel":          hotel["_id"],
+		"Price":          hotel["price"],
 	}
 
 	c.IndentedJSON(http.StatusOK, response)
